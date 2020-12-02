f6efd5f3c439eccf16cbd0673a08a99004ece473
mapokapo <leopetrovic11@gmail.com>
2020-12-02 23:07:38 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+import styles from "../shared/styles/Home.module.css";
+
+const Home: NextPage = () => {
+	return <div className={styles.container}></div>;
+};
+
+export default Home;
