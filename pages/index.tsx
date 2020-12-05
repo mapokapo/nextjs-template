c6fc038285307c94864a39b8b740c405c0ae2611
mapokapo <leopetrovic11@gmail.com>
2020-12-05 16:15:07 +0100

    Added TailwindCSS, Stylelint, a basic webpage structure, and a license

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from "next";
 
-import styles from "../shared/styles/Home.module.css";
-
 const Home: NextPage = () => {
-	return <div className={styles.container}></div>;
+	return (
+		<>
+			<div className="w-3/4 m-auto bg-green-300 h-96"></div>
+			<div className="w-1/12 sm:w-16 md:h-16"></div>
+			<div className="w-3/4 m-auto bg-green-300 h-96"></div>
+		</>
+	);
 };
 
 export default Home;
